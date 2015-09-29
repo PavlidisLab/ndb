d3e3e876fcb27b50ceea359081006d43ca3d9749
Manuel Belmadani <mbelm006@uottawa.ca>
2015-09-29 15:45:29 -0700

    added Papers DAO, DTO, class and service. Then merged.

---
+++ src/main/java/ubc/pavlab/ndb/beans/services/PaperService.java
@@ -0,0 +1,121 @@
+/*
+ * The ndb project
+ * 
+ * Copyright (c) 2015 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package ubc.pavlab.ndb.beans.services;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import org.apache.log4j.Logger;
+
+import ubc.pavlab.ndb.beans.DAOFactoryBean;
+import ubc.pavlab.ndb.dao.PaperDAO;
+import ubc.pavlab.ndb.exceptions.DAOException;
+import ubc.pavlab.ndb.model.Paper;
+import ubc.pavlab.ndb.model.Paper.PaperBuilder;
+import ubc.pavlab.ndb.model.dto.PaperDTO;
+
+/**
+ * Service layer on top of GeneDAO. Contains methods for fetching information related to genes from the database and
+ * creating Gene objects.
+ * 
+ * @author mjacobson
+ * @version $Id$
+ */
+@ManagedBean
+@ApplicationScoped
+public class PaperService implements Serializable {
+
+    private static final long serialVersionUID = 3813762194987799804L;
+
+    private static final Logger log = Logger.getLogger( PaperService.class );
+
+    @ManagedProperty("#{daoFactoryBean}")
+    private DAOFactoryBean daoFactoryBean;
+
+    private PaperDAO paperDAO;
+
+    /**
+     * 
+     */
+    public PaperService() {
+        log.info( "PaperService created" );
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info( "PaperService init" );
+        paperDAO = daoFactoryBean.getDAOFactory().getPaperDAO();
+
+    }
+
+    /**
+     * Returns the paper from the database matching the given ID, otherwise null.
+     * 
+     * @param id The ID of the paper to be returned.
+     * @return The paper from the database matching the given ID, otherwise null.
+     * @throws DAOException If something fails at database level.
+     */
+    public Paper fetchPaper( Integer id ) {
+        return map( paperDAO.find( id ) );
+    }
+
+    /**
+     * Returns the paper from the database matching the given author, otherwise null.
+     * 
+     * @param The author of the paper to be returned.
+     * @return The paper from the database matching the given author, otherwise null.
+     * @throws DAOException If something fails at database level.
+     */
+    public Paper fetchPaper( String author ) {
+        return map( paperDAO.find( author ) );
+    }
+
+    /**
+     * Returns a list of all papers from the database ordered by paper ID. The list is never null and is empty when the
+     * database does not contain any papers.
+     * 
+     * @return A list of all papers from the database ordered by paper ID.
+     * @throws DAOException If something fails at database level.
+     */
+    public List<Paper> listPapers() {
+        List<Paper> paperList = new ArrayList<>();
+        for ( PaperDTO dto : paperDAO.list() ) {
+            paperList.add( map( dto ) );
+        }
+        return paperList;
+    }
+
+    private static Paper map( PaperDTO dto ) {
+        PaperBuilder builder = new PaperBuilder( dto.getId(), dto.getAuthor(), dto.getPaper_table(),
+                dto.getMut_reporting(), dto.getScope(), dto.isParents(), dto.getCohort(), dto.getCohort_source(),
+                dto.getCohort_size(), dto.getReported_effects() );
+        return builder.build();
+    }
+
+    public void setDaoFactoryBean( DAOFactoryBean daoFactoryBean ) {
+        this.daoFactoryBean = daoFactoryBean;
+    }
+}
