65b770923e8b88421ce294103b9385e3cc5d3461
Manuel Belmadani <mbelm006@uottawa.ca>
2015-10-26 11:26:45 -0700

    updates to UI

--- src/main/java/ubc/pavlab/ndb/model/Paper.java
+++ src/main/java/ubc/pavlab/ndb/model/Paper.java
@@ -1,13 +1,22 @@
 package ubc.pavlab.ndb.model;
 
 public final class Paper {
-    private final String author;
+    private final String url;
+    private final String name;
 
-    public String getName() {
-        return author;
+    public Paper( String name, String url ) {
+        this.name = name;
+        this.url = url;
     }
 
-    public Paper( String author ) {
-        this.author = author;
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        if ( this.url == null ) {
+            return "#"; // So it doesn't create a borked link.
+        }
+        return url;
     }
 }
