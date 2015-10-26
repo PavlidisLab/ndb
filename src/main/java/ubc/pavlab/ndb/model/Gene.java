79c0a6187a74c86ac9a6a1de947410c8824c531a
Manuel Belmadani <mbelm006@uottawa.ca>
2015-10-26 11:26:45 -0700

    updates to UI

--- src/main/java/ubc/pavlab/ndb/model/Gene.java
+++ src/main/java/ubc/pavlab/ndb/model/Gene.java
@@ -2,12 +2,18 @@ package ubc.pavlab.ndb.model;
 
 public final class Gene {
     private final String name;
+    private final String ensembl_id;
 
     public String getName() {
         return name;
     }
 
-    public Gene( String name ) {
+    public Gene( String name, String ensembl_id ) {
         this.name = name;
+        this.ensembl_id = ensembl_id;
+    }
+
+    public String getEnsembl_id() {
+        return ensembl_id;
     }
 }
--- src/main/java/ubc/pavlab/ndb/service/MutationService.java
+++ src/main/java/ubc/pavlab/ndb/service/MutationService.java
@@ -15,29 +15,35 @@ public class MutationService {
 
         for ( int i = 0; i < 50; i++ ) {
             String idx = Integer.toString( i / 5 );
-            list.add( new Mutation( i++, new Paper( "paper" + idx ), new Gene( "gene1" + idx ), "sampleIdentifier",
-                    "chromosome12" + idx, 12837123 + i, 12837128 + i, "AAT", "-", "Missense", "GarbleGableCodeChange",
-                    "garblegarbleprotchange", "0.9", "No idea", "Maybe", true ) );
+            list.add( new Mutation( i++,
+                    new Paper( "Jiang YH et al. Am J Hum Genet. 2013",
+                            "http://www.ncbi.nlm.nih.gov/pubmed/?term=23849776" ),
+                    new Gene( "SYNGAP1", "ENSG00000197283" ), "sampleIdentifier", "6", 33387847, 33421466, "AAT", "-",
+                    "Missense", "GarbleGableCodeChange", "garblegarbleprotchange", "0.9", "No idea", "Maybe", true ) );
 
             idx = Integer.toString( i );
-            list.add( new Mutation( i++, new Paper( "paper" + idx ), new Gene( "gene1" + idx ), "sampleIdentifier",
-                    "chromosome12" + idx, 12837 + i, 12837 + i, "CA", "G", "Missense", "GarbleGableCodeChange",
-                    "garblegarbleprotchange", "0.4", "No idea", "Maybe", true ) );
+            list.add( new Mutation( i++, new Paper( "paper" + idx, null ),
+                    new Gene( "gene1" + idx, "ENSG0000019728" + idx ), "sampleIdentifier", "chromosome12" + idx,
+                    12837 + i, 12837 + i, "CA", "G", "Missense", "GarbleGableCodeChange", "garblegarbleprotchange",
+                    "0.4", "No idea", "Maybe", true ) );
 
             idx = Integer.toString( i );
-            list.add( new Mutation( i++, new Paper( "paper" + idx ), new Gene( "gene1" + idx ), "sampleIdentifier",
-                    "chromosome12" + idx, 12837123, 12837128, "C", "GAA", "Missense", "GarbleGableCodeChange",
-                    "garblegarbleprotchange", "0.3", "No idea", "Maybe", true ) );
+            list.add( new Mutation( i++, new Paper( "paper" + idx, null ),
+                    new Gene( "gene1" + idx, "ENSG0000019728" + idx ), "sampleIdentifier", "chromosome12" + idx,
+                    12837123, 12837128, "C", "GAA", "Missense", "GarbleGableCodeChange", "garblegarbleprotchange",
+                    "0.3", "No idea", "Maybe", true ) );
 
             idx = Integer.toString( i );
-            list.add( new Mutation( i++, new Paper( "paper" + idx ), new Gene( "gene1" + idx ), "sampleIdentifier",
-                    "chromosome12" + idx, 35234634, 334534636, "-", "G", "Missense", "GarbleGableCodeChange",
-                    "garblegarbleprotchange", "0.1", "No idea", "Maybe", true ) );
+            list.add( new Mutation( i++, new Paper( "paper" + idx, null ),
+                    new Gene( "gene1" + idx, "ENSG0000019728" + idx ), "sampleIdentifier", "chromosome12" + idx,
+                    35234634, 334534636, "-", "G", "Missense", "GarbleGableCodeChange", "garblegarbleprotchange", "0.1",
+                    "No idea", "Maybe", true ) );
 
             idx = Integer.toString( i );
-            list.add( new Mutation( i++, new Paper( "paper" + idx ), new Gene( "gene1" + idx ), "sampleIdentifier",
-                    "chromosome12" + idx, 123233 + i, 123233 + i, "C", "G", "Missense", "GarbleGableCodeChange",
-                    "garblegarbleprotchange", "0.9", "No idea", "Maybe", true ) );
+            list.add( new Mutation( i++, new Paper( "paper" + idx, null ),
+                    new Gene( "gene1" + idx, "ENSG0000019728" + idx ), "sampleIdentifier", "chromosome12" + idx,
+                    123233 + i, 123233 + i, "C", "G", "Missense", "GarbleGableCodeChange", "garblegarbleprotchange",
+                    "0.9", "No idea", "Maybe", true ) );
         }
         return list;
     }
@@ -53,8 +59,9 @@ public class MutationService {
         if ( idx == 6 ) {
             for ( int i = 0; i < 10; i++ ) {
 
-                list.add( new Mutation( i, new Paper( "paper" + i ), new Gene( "gene1" + idx ), "sampleIdentifier",
-                        "chromosome12", 12837123 + i, 12837128 + i, "AAT", "-", "Missense", "GarbleGableCodeChange",
+                list.add( new Mutation( i, new Paper( "paper" + i, null ),
+                        new Gene( "gene1" + idx, "ENSG0000019728" + idx ), "sampleIdentifier", "chromosome12",
+                        12837123 + i, 12837128 + i, "AAT", "-", "Missense", "GarbleGableCodeChange",
                         "garblegarbleprotchange", "0.9", "No idea", "Maybe", true ) );
             }
         }
@@ -65,8 +72,9 @@ public class MutationService {
         Mutation trunk = null;
         // TODO: Return appropriate trunk mutation
         if ( idx == 6 ) {
-            trunk = new Mutation( idx, new Paper( "paper" + idx ), new Gene( "gene1" + idx ), "sampleIdentifier",
-                    "chromosome12", 12837123 + idx, 12837128 + idx, "AAT", "-", "Missense", "GarbleGableCodeChange",
+            trunk = new Mutation( idx, new Paper( "paper" + idx, null ),
+                    new Gene( "gene1" + idx, "ENSG0000019728" + idx ), "sampleIdentifier", "chromosome12",
+                    12837123 + idx, 12837128 + idx, "AAT", "-", "Missense", "GarbleGableCodeChange",
                     "garblegarbleprotchange", "0.9", "No idea", "Maybe", true );
         }
 
@@ -82,7 +90,7 @@ public class MutationService {
         // ( Integer.parseInt( idx ) == 0 ) {
         // Hashtable<String, String> kv = new Hashtable<String, String>();
         String idx = "0";
-        raw = new SourceMutation( Integer.parseInt( idx ), new Paper( "paper" + idx ), null );
+        raw = new SourceMutation( Integer.parseInt( idx ), new Paper( "paper" + idx, null ), null );
         // }
 
         return raw;
