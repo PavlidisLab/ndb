9afa69b25bea4b95ec88072ea4a4adb4a96e7ebb
JacobsonMT <JacobsonMT+github@gmail.com>
2015-12-02 13:39:02 -0800

    New Theme, search bar on main template instead of index

--- src/main/java/ubc/pavlab/ndb/beans/IndexView.java
+++ src/main/java/ubc/pavlab/ndb/beans/IndexView.java
@@ -20,18 +20,12 @@
 package ubc.pavlab.ndb.beans;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 import org.apache.log4j.Logger;
-
-import ubc.pavlab.ndb.beans.services.CacheService;
-import ubc.pavlab.ndb.model.Gene;
-import ubc.pavlab.ndb.model.Paper;
 
 /**
  * TODO Document Me
@@ -46,14 +40,6 @@ public class IndexView implements Serializable {
     private static final long serialVersionUID = -2394751566929159597L;
     private static final Logger log = Logger.getLogger( IndexView.class );
 
-    @ManagedProperty("#{cacheService}")
-    private CacheService cacheService;
-
-    private Integer geneId;
-    private String chromosome;
-    private Integer start;
-    private Integer stop;
-
     public IndexView() {
         log.info( "IndexView created" );
 
@@ -63,60 +49,6 @@ public class IndexView implements Serializable {
     public void postConstruct() {
         log.info( "IndexView postConstruct" );
 
-    }
-
-    public String searchByGeneSymbol() {
-        return "variant?faces-redirect=true&NCBIGeneId=" + geneId;
-    }
-
-    public String searchByCoordinates() {
-        return "variant?faces-redirect=true&chr=" + chromosome + "&start=" + start + "&stop=" + stop;
-    }
-
-    public List<Gene> completeSymbol( String query ) {
-        log.info( query );
-        return cacheService.searchGeneBySymbol( query );
-    }
-
-    public List<Paper> completeAuthor( String query ) {
-        log.info( query );
-        return cacheService.searchPaperByAuthor( query );
-    }
-
-    public Integer getGeneId() {
-        return geneId;
-    }
-
-    public void setGeneId( Integer geneId ) {
-        this.geneId = geneId;
-    }
-
-    public String getChromosome() {
-        return chromosome;
-    }
-
-    public void setChromosome( String chromosome ) {
-        this.chromosome = chromosome;
-    }
-
-    public Integer getStart() {
-        return start;
-    }
-
-    public void setStart( Integer start ) {
-        this.start = start;
-    }
-
-    public Integer getStop() {
-        return stop;
-    }
-
-    public void setStop( Integer stop ) {
-        this.stop = stop;
-    }
-
-    public void setCacheService( CacheService cacheService ) {
-        this.cacheService = cacheService;
     }
 
 }
