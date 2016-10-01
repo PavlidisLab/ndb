7389a1c888ef15627dc0a85470e73f07d7a69b4b
Manuel Belmadani <manuel.belmadani@gmail.com>
2016-09-30 17:49:30 -0700

    Added fixes to import the Tammimies paper.

--- etc/paperpipe/scripts/models/fixer/fixer.py
+++ etc/paperpipe/scripts/models/fixer/fixer.py
@@ -5,8 +5,13 @@ class Fixer(object):
         start_str = "-start={0}".format(str(start - 1))  # 0 based
         stop_str = "-end={0}".format(str(stop))  # Don't remove 1 here because it is [start,stop)
         chromosome_str = "-seq=chr{0}".format(chromosome)
+        if chromosome_str == "-seq=chr23":
+            chromosome_str = "-seq=chrX"
+        elif chromosome_str == "-seq=chr24":
+            chromosome_str = "-seq=chrY"
         exe = "/home/mbelmadani/development/ndb/etc/paperpipe/scripts/models/fixer/twoBitToFa"
         genome = "/home/mbelmadani/development/ndb/etc/paperpipe/scripts/models/fixer/hg19.2bit"
+
         fix =  check_output(  [ exe, genome, 'stdout', chromosome_str, start_str, stop_str ]  )
         return "".join( fix.split('\n')[1:] )
 
@@ -16,41 +21,57 @@ class Fixer(object):
         1 - If ref and alt are none, then error, we can't repair this.
         2 - If ref is empty but not alt (insertion), then decrease start position by 1, and prepend base to ref and alt
         3 - If alt is empty but not ref (deletion), the decreate start position by 1, and prepend base to ref and alt.
-        4 - Otherwise, don't touch it, but run sanity check anyways.
+        4 - If stop is missing, then compute using the distance from start + ref
+
+        ---- Otherwise, don't touch it, but run sanity check anyways.
 
         """
         start_, ref_, alt_ = start, ref, alt
-        empty = [None, '']
+        stop_ = stop
+
+        empty = [None, '', "-", "0"]
 
         if ref == alt and ref in empty:
             """ The unrepairable case """
-
             print "Error: ref and alt cannot both be empty..."
-            raise ValueError
+            raise ValueError        
+        elif ref == alt and ref == "N":
+            """ N == ref == alt means that we have to skip this variant."""
+            return None, None, None, ref, alt
         elif (ref in empty) or (alt in empty):
             """ The variant needs an anchor. """
+            
+            # Get rid of non-nucleotides
+            if ref in empty:
+                ref_ = self.get_ref(chromosome, start_, start_)
+            if alt in empty:
+                alt_ = self.get_ref(chromosome, start_, start_)
+
             start_ = start - 1
             nc = self.get_ref(chromosome, start_, start_)
             ref_ = str(nc + ref_).upper()
             alt_ = str(nc + alt_).upper()
 
+        if stop_ in [None, '', 0]:
+            stop_ = start_ + (len(ref_) - 1)
+
         try:
             #Sanity check that variant ref matches reference genome.
-            expected = self.get_ref(chromosome, start_, stop)
+            expected = self.get_ref(chromosome, start_, stop_)
             if expected != ref_:
                 raise ValueError("Excepted is not the same between reference genome and current variant: " + expected + " v.s. " + ref_)        
         except ValueError as v:
             print v
             print "Something went wrong when trying to match reference variant to reference genome."
-            print "chomosome:", chromosome
+            print "chromosome:", chromosome
             print "start:", start_
-            print "stop:", stop
+            print "stop:", stop_
             print "ref:", ref_
             print "alt:", alt_
             raise v
 
         # Everything looks good.
-        return chromosome, start_, stop, ref_, alt_
+        return chromosome, start_, stop_, ref_, alt_
 
 if __name__ == "__main__":
     import sys
