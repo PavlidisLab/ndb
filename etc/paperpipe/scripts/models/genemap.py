2ca0743a3b9bc5c0661374e900331d380abfe179
mbelmadani <manuel.belmadani@gmail.com>
2018-03-19 14:17:01 -0700

    Added known gene mapping to help transition annovar to NCBI genes. Mostly LOCs, unmapped contigs etc.

--- etc/paperpipe/scripts/models/genemap.py
+++ etc/paperpipe/scripts/models/genemap.py
@@ -2,3 +2,15 @@ annovar_to_ncbi = {}
 
 annovar_to_ncbi['EnterAnnovarGene']='CorrectNCBIGene'
 annovar_to_ncbi['KIAA1804']='MAP3K21'
+annovar_to_ncbi['FLJ33360']='LINC02145'
+annovar_to_ncbi['FLJ33581']='LINC01721'
+annovar_to_ncbi['FLJ46066']='LINC01994'
+annovar_to_ncbi['FLJ26245']='LINC02167'
+annovar_to_ncbi['LINC00273']='' # Reported as a cloning artifact thus removed from HGNC;
+annovar_to_ncbi['FLJ43879']='LINC01940'
+annovar_to_ncbi['ZNF664-FAM101A']='' # Read-through.
+annovar_to_ncbi['FLJ41200']='LINC01235'
+annovar_to_ncbi['FLJ41278']='LINC02389'
+annovar_to_ncbi['FLJ45079']='LINC01973'
+annovar_to_ncbi['FLJ26850']='LOC400710'
+annovar_to_ncbi['FLJ16171']='LINC01951'
