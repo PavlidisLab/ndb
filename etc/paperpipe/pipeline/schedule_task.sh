4071ae26b7768e87e2dc6562268306382493a4c3
mbelmadani <manuel.belmadani@gmail.com>
2019-11-07 19:38:44 -0800

    Changed interactive switch to silent to make the more common usage default.

--- etc/paperpipe/pipeline/schedule_task.sh
+++ etc/paperpipe/pipeline/schedule_task.sh
@@ -15,12 +15,12 @@ if [ "$#" -lt "2" ]
     exit
 fi
 
-INTERACTIVE=0
+INTERACTIVE=1
 if [ "$#" -gt "2" ]
   then
-    if [ "$3" == "--interactive" ]
+    if [ "$3" == "--silent" ]
     then
-	INTERACTIVE=1
+	INTERACTIVE=0
     else
 	echo "Error! Unknown switch $3"
 	exit -1
@@ -33,11 +33,6 @@ JOB=$2
 PAPERNAME=$(echo $BOOK | rev | cut -f1 -d"/" | cut -d"." -f2 | rev )
 
 echo "Loading book: $BOOK"
-#PYTHONPATH='flows:../scripts/models' luigi --module main LoadPaper --book $BOOK
-#PYTHONPATH='flows:../scripts/models' luigi --module main LoadRawKV --book $BOOK
-#PYTHONPATH='flows:../scripts/models' luigi --module main LoadPaper --book $1 --local-scheduler
-
-
 
 if [ $INTERACTIVE -eq 0  ];
 then
