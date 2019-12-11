78591f66b841ed2190d988fa0e5096b46242b0a8
mbelmadani <manuel.belmadani@gmail.com>
2019-12-10 21:48:37 -0800

    Removed unused files.

--- etc/paperpipe/pipeline/clearall.sh
+++
@@ -1,6 +0,0 @@
-./schedule_task.sh $1 ClearAnnovar &&
-./schedule_task.sh $1 ClearVariant &&
-./schedule_task.sh $1 ClearRawVariant &&
-./schedule_task.sh $1 ClearRawKV &&
-./schedule_task.sh $1 ClearPaper
-
