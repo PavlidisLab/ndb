0e1bbddf3f81ca51d4401b40e222ed7ffb8ed7cf
Manuel Belmadani <manuel.belmadani+RUSH_TMT_dsk@gmail.com>
2020-03-21 13:09:54 -0400

    Added IdleMonitorView to manage sessions.

---
+++ src/main/java/ubc/pavlab/ndb/beans/IdleMonitorView.java
@@ -0,0 +1,33 @@
+package ubc.pavlab.ndb.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean(name="idleMonitorView")
+@SessionScoped
+public class IdleMonitorView {
+
+    //static boolean expired = false;
+    private static int maxInterval;
+
+    public void onIdleWarning() {
+        FacesContext.getCurrentInstance().addMessage("messagesWarn", new FacesMessage(FacesMessage.SEVERITY_WARN,
+                "No activity.", "Your session will expire soon due to inactivity."));
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+        maxInterval = session.getMaxInactiveInterval();
+        session.setMaxInactiveInterval(30 * 1); // One half minute
+    }
+
+    public void onActiveWarning() {
+        FacesContext.getCurrentInstance().addMessage("messagesWarn", new FacesMessage(FacesMessage.SEVERITY_INFO,
+                "Welcome Back", "Your session has been preserved."));
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+        session.setMaxInactiveInterval(maxInterval);
+    }
+}
